5e4426987ac60adc90581e772eaade42eff4c6b0
VK-2-9 <vishalnath002@gmail.com>
2025-04-27 15:38:33 +0530

    complete

--- index.js
+++ index.js
@@ -2,24 +2,40 @@ const express=require("express")
 const cors =require("cors")
 
 const app= express()
+const userArr=[
+    {
+        uName:"vk",
+        uPass:"123",
+    },
+    {
+        uName:"ak",
+        uPass:"456"
+    }
+]
 
-var user="vk"
-var pass=123
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 
 app.post("/login",(req,res)=>{
+    var result=false
     console.log(req.body.uName)
-    if(req.body.uName===user && req.body.uPass==pass){
-        res.send(true)
-    }
-    else{
-        res.send(false)
-    }
+    userArr.forEach((item)=>{
+        if(req.body.uName===item.uName && req.body.uPass==item.uPass){
+            result=true
+        }
+        
+    })
+   res.send(result)
     
 }
 )
+
+app.post("/signup",(req,res)=>{
+    userArr.push({ uName: req.body.uName, uPass: req.body.uPass });
+    res.send(true)
+    console.log(userArr)
+})
 
 app.listen(5000,()=>{
     console.log("server startedcd ")
